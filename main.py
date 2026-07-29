ddde2fe22c4d7d9a0818e7ad24cc12aa2c55e9a5
cwolfe1080 <cewolfe19@gmail.com>
2026-07-28 22:34:50 -0400

    Set up user login

--- main.py
+++ main.py
@@ -11,14 +11,34 @@ def execute():
     # Login to scratch using provided credentials
     session = sa.login(f"{uname}", f"{pswd}") #Returns a sa.Session object
 
+    # Refresh account data
+    print("Fetching account data...")
+    session.update()
+
+
     # Check to see if the account has a verified email. Accounts without verified emails cannot post comments
-    user.update()
     print("Checking for account email verification...")
-    if not session.has_outstanding_email_confirmation :
+    if not session.email:
         print("Please verify the email address associated with this account, or use another account with a verified email address.")
         exit()
     else:
         pass
+    # Check to see if account is banned
+    print("Checking if account is banned...")
+    if session.banned:
+        print("The Scratch account that you signed in with is banned.")
+    else:
+        pass
+
+    # Sign in the user using the session data above
+    print("Connecting to user...")
+    user = session.connect_linked_usr()
+    # Fetch new user data
+    print("Fetching user data...")
+    user.update()
+
+    # NOTE: I'm not sure what the difference between the user and session things are. I'm pretty sure that user is needed for comments and things like that, whereas session is needed for cloud variables. Not sure that I need to login the session, but oh well, maybe we'll need it later in the project, but for now I'm going to leave it there.
+
     
         
 
